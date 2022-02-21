01aa66aafb600ebe82dd11d8f4920d83acd1586a
Revand Mika <82688238+RevandDev@users.noreply.github.com>
2022-02-21 17:41:10 +0700

    Create Main.php

---
+++ src/Rev/BuyHealFeed/Main.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Rev\BuyHealFeed;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\{Event, Listener};
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\utils\{TextFormat, Config};
+
+class Main extends PluginBase implements Listener {
+    
+    public function onEnable() : void{
+        $this->saveDefaultConfig();
+        $this->reloadConfig();
+        @mkdir($this->getDataFolder());
+        $this->saveResource("config.yml");
+        $this->eco = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+    }
+    
+    public function onCommand(CommandSender $p, Command $cmd, String $label, array $args): bool{
+         if ($p instanceof Player) {
+             if ($cmd->getName() === 'buyheal') {
+                if ($this->eco->myMoney($p) >= $this->getConfig()->get("heal-price")) {
+                    $msg = str_replace("{name}", $p->getName(), $this->getConfig()->get("heal-succes-msg"));
+                    $this->eco->reduceMoney($p, $this->getConfig()->get("heal-price"));
+                    $p->setHealth($p->getMaxHealth());
+                    $p->sendMessage($msg);
+                } else {
+                    $msg = str_replace("{name}", $p->getName(), $this->getConfig()->get("heal-failed-msg"));
+                    $p->sendMessage($msg);
+                }
+             } elseif ($cmd->getName() === 'buyfeed') {
+                if ($this->eco->myMoney($p) >= $this->getConfig()->get("feed-price")) {
+                    $msg = str_replace("{name}", $p->getName(), $this->getConfig()->get("feed-succes-msg"));
+                    $this->eco->reduceMoney($p, $this->getConfig()->get("feed-price"));
+                    $p->getHungerManager()->setFood(20);
+                    $p->getHungerManager()->setSaturation(20);
+                    $p->sendMessage($msg);
+                } else {
+                    $msg = str_replace("{name}", $p->getName(), $this->getConfig()->get("feed-failed-msg"));
+                    $p->sendMessage($msg);
+                }
+             }
+         } else {
+             $msg = $this->getConfig()->get("ingame-msg");
+             $p->sendMessage($msg);
+         }
+             
+        return true;
+    }
+}
